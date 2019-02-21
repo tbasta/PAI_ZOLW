bdc22eed944a6a1cb674ad4ed8d0b07d6ed718ae
tbasta <basta.tomek1994@gmail.com>
2019-02-21 21:56:04 +0100

    Add files via upload

--- Brython/src/async.js
+++ Brython/src/async.js
@@ -1,1 +1,39 @@
+;(function($B) {
+var _b_ = $B.builtins
+var awaitable = $B.make_class("awaitable")
+var coroutine = $B.coroutine = $B.make_class("coroutine")
 
+coroutine.close = function(self){}
+coroutine.send = function(self){
+    return self.$func.apply(null, self.$args)
+}
+
+$B.set_func_names(coroutine, "builtins")
+
+$B.make_async = function(func){
+    var f = function(){
+        var args = arguments
+        return {
+            __class__: coroutine,
+            $args: args,
+            $func: func
+        }
+    }
+    f.$infos = func.$infos
+    return f
+}
+
+$B.promise = function(obj){
+    console.log("promise", obj)
+    if(obj.__class__ === $B.JSObject){
+        return obj.js
+    }else if(obj.__class__ === coroutine){
+        return coroutine.send(obj)
+    }
+    if(typeof obj == "function"){
+        return obj()
+    }
+    return obj
+}
+
+})(__BRYTHON__)
