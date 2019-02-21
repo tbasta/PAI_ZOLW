d2931d0ebf5b166f1b8ce2d9c1605dc41c7b9ecf
tbasta <basta.tomek1994@gmail.com>
2019-02-21 21:52:31 +0100

    Add files via upload

---
+++ Brython/Brython.py
@@ -0,0 +1,24 @@
+from browser import document, alert
+from turtle import restart
+import turtle
+
+restart()
+
+turtle.set_defaults(turtle_canvas_wrapper = document['lowerContainer'], canvwidth = 1000, canvheight = 800)
+t = turtle.Turtle()
+t.penup()
+t.goto(0, 0)
+t.width(5)
+t.pendown()
+
+try:
+    _code
+    turtle.done()
+except:
+    try:
+        traceback.print_exc()
+        alert("Nieprawidłowy kod!")
+    except:
+        alert("Nie można wykonać kodu")
+
+document["mybutton"].bind("click", runcode)
---
+++ Brython/module1.py
@@ -0,0 +1,24 @@
+import sys
+import traceback
+
+cmd = ""
+
+def create_command(_cmd):
+    cmd = open('Brython.py').read()
+    print("było: " + cmd)
+    cmd = cmd.replace("_code", _cmd)
+    print("jest: " + cmd)
+    return cmd
+    
+def exec_code():
+    _code = create_command("some text")
+    try:
+        exec(_code)
+        print("finalnie: " + _code)
+    except:
+        try:
+            traceback.print_exc()
+        except:
+            print("could not print traceback")
+
+exec_code()
